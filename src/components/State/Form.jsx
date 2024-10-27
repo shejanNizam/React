d56a2dc8d3a404c9cb245eff06630fad05b338ba
shejanNizam <nizam.shejan11@gmail.com>
2024-10-28 04:24:52 +0600

    object update in state

--- src/components/State/Form.jsx
+++ src/components/State/Form.jsx
@@ -1,33 +1,122 @@
+// import { useState } from "react";
+
+// export default function Form() {
+//   const [to, setTo] = useState("Alice");
+//   const [message, setMessage] = useState("Hello");
+
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     setTimeout(() => {
+//       alert(`You said ${message} to ${to}`);
+//     }, 5000);
+//   }
+
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <label>
+//         To:{" "}
+//         <select value={to} onChange={(e) => setTo(e.target.value)}>
+//           <option value="Alice">Alice</option>
+//           <option value="Bob">Bob</option>
+//         </select>
+//       </label>
+<br />;
+<br />;
+//       <br />
+//       <textarea
+//         placeholder="Message"
+//         value={message}
+//         onChange={(e) => setMessage(e.target.value)}
+//       />
+//       <br />
+//       <button type="submit">Send</button>
+//     </form>
+//   );
+// }
+
 import { useState } from "react";
 
 export default function Form() {
-  const [to, setTo] = useState("Alice");
-  const [message, setMessage] = useState("Hello");
+  const [person, setPerson] = useState({
+    name: "Niki de Saint Phalle",
+    artwork: {
+      title: "Blue Nana",
+      city: "Hamburg",
+      image: "https://i.imgur.com/Sd1AgUOm.jpg",
+    },
+  });
 
-  function handleSubmit(e) {
-    e.preventDefault();
-    setTimeout(() => {
-      alert(`You said ${message} to ${to}`);
-    }, 5000);
+  function handleNameChange(e) {
+    setPerson({
+      ...person,
+      name: e.target.value,
+    });
+  }
+
+  function handleTitleChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        title: e.target.value,
+      },
+    });
+  }
+
+  function handleCityChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        city: e.target.value,
+      },
+    });
+  }
+
+  function handleImageChange(e) {
+    setPerson({
+      ...person,
+      artwork: {
+        ...person.artwork,
+        image: e.target.value,
+      },
+    });
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <>
       <label>
-        To:{" "}
-        <select value={to} onChange={(e) => setTo(e.target.value)}>
-          <option value="Alice">Alice</option>
-          <option value="Bob">Bob</option>
-        </select>
+        Name:
+        <input value={person.name} onChange={handleNameChange} />
       </label>
       <br />
-      <textarea
-        placeholder="Message"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
       <br />
-      <button type="submit">Send</button>
-    </form>
+      <label>
+        Title:
+        <input value={person.artwork.title} onChange={handleTitleChange} />
+      </label>
+      <br />
+      <br />
+      <label>
+        City:
+        <input value={person.artwork.city} onChange={handleCityChange} />
+      </label>
+      <br />
+      <br />
+      <label>
+        Image:
+        <input value={person.artwork.image} onChange={handleImageChange} />
+      </label>
+      <br />
+      <br />
+      <p>
+        <i>{person.artwork.title}</i>
+        {" by "}
+        {person.name}
+        <br />
+        (located in {person.artwork.city})
+      </p>
+      <img src={person.artwork.image} alt={person.artwork.title} />
+    </>
   );
 }
