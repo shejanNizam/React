aa2624c166f65da7f63c09a712c0648ae3f93c6d
shejanNizam <nizam.shejan11@gmail.com>
2024-10-28 03:08:37 +0600

    counter in state

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Gallery from "./components/State/Gallery";
+import Counter from "./components/State/Counter";
 
 export default function App() {
   return (
     <>
       <h2>React App</h2>
-      <Gallery />
+      <Counter />
     </>
   );
 }
