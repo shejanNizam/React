59bbc81f86e03e31a145c5405e53666d0ec8e678
shejanNizam <nizam.shejan11@gmail.com>
2024-10-28 14:49:33 +0600

    removing from an array

--- src/components/State/List.jsx
+++ src/components/State/List.jsx
@@ -1,25 +1,60 @@
+//adding array ----------------
+
+// import { useState } from "react";
+
+// let nextId = 0;
+
+// export default function List() {
+//   const [name, setName] = useState("");
+//   const [artists, setArtists] = useState([]);
+
+//   return (
+//     <>
+//       <h1>Inspiring sculptors:</h1>
+//       <input value={name} onChange={(e) => setName(e.target.value)} />
+//       <button
+//         onClick={() => {
+//           setArtists([...artists, { id: nextId++, name: name }]);
+//         }}
+//       >
+//         Add
+//       </button>
+//       <ul>
+//         {artists.map((artist) => (
+//           <li key={artist.id}>{artist.name}</li>
+//         ))}
+//       </ul>
+//     </>
+//   );
+// }
+
+// removing from an array
 import { useState } from "react";
 
-let nextId = 0;
+let initialArtists = [
+  { id: 0, name: "Marta Colvin Andrade" },
+  { id: 1, name: "Lamidi Olonade Fakeye" },
+  { id: 2, name: "Louise Nevelson" },
+];
 
 export default function List() {
-  const [name, setName] = useState("");
-  const [artists, setArtists] = useState([]);
+  const [artists, setArtists] = useState(initialArtists);
 
   return (
     <>
       <h1>Inspiring sculptors:</h1>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <button
-        onClick={() => {
-          setArtists([...artists, { id: nextId++, name: name }]);
-        }}
-      >
-        Add
-      </button>
       <ul>
         {artists.map((artist) => (
-          <li key={artist.id}>{artist.name}</li>
+          <li key={artist.id}>
+            {artist.name}{" "}
+            <button
+              onClick={() => {
+                setArtists(artists.filter((a) => a.id !== artist.id));
+              }}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
