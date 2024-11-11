15256511c153e19e6dd9543f4ab0f4f0ee6ac2a1
shejanNizam <nizam.shejan11@gmail.com>
2024-11-11 15:58:56 +0600

    useEffect cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import Video2 from "./components/EscapeHatches/effects/Video2";
+import ChatRoom from "./components/EscapeHatches/effects/ChatRoom";
 
 export default function App() {
   return (
     <>
       <h2>React App</h2>
-      <Video2 />
+      <ChatRoom />
     </>
   );
 }
