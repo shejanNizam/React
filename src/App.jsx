e3eaf2b17299ea557bd2036ddafb0957afe0a010
shejanNizam <nizam.shejan11@gmail.com>
2024-11-11 06:57:39 +0600

    DOM with ref

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import Form from "./components/EscapeHatches/Form";
+import CatFriends from "./components/EscapeHatches/CatFriends";
 
 export default function App() {
   return (
     <>
       <h2>React App</h2>
-      <Form />
+      <CatFriends />
     </>
   );
 }
