52600288570e5909da37bb001aff1e3f9d6f87ba
shejanNizam <nizam.shejan11@gmail.com>
2024-11-11 11:54:27 +0600

    useEffect with dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import "./App.css";
-import Video from "./components/EscapeHatches/effects/Video";
+import Video2 from "./components/EscapeHatches/effects/Video2";
 
 export default function App() {
   return (
     <>
       <h2>React App</h2>
-      <Video />
+      <Video2 />
     </>
   );
 }
